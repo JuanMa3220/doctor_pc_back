b16aa99c30f00378de1dc610f144e0a5b9f95d20
Juan Manuel Hurtado <juanma0223@hotmail.com>
2023-05-03 18:31:09 -0500

    Configuraciones para despliegue en Onrender

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 // Base de datos
 var dataSource = new typeorm.DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '1234',
-    database: 'doctor_pc',
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     entities: [
         require('./entities/Roles.js'),
